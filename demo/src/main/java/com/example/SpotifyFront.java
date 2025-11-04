fa4ce6afe39abb2c3905f90ad41ab40aa1ab3cf1
TrieuThien2007 <nguyentrieuthien2906@gmail.com>
2025-11-03 17:54:12 -0800

    Code For Interface

--- demo/src/main/java/com/example/SpotifyFront.java
+++ demo/src/main/java/com/example/SpotifyFront.java
@@ -5,6 +5,19 @@ import java.awt.*;
 import java.io.*;
 import java.util.*;
 
-public class SpotifyFront {
-    
+public class SpotifyFront extends JFrame {
+    private Clip clip;
+    private JList<String> list;
+    private DefaultListModel<String> model;
+    private JTextArea comment;
+    private JLabel status;
+    private Map<String, String>comments = new HashMap<>();
+    private Set<String> fav = new HashSet<>();
+    private String dir ="F://Song For Spotify App//Song//wav";
+    public SpotifyFront(){
+        setTitle(" Spotify App Interface");
+        setSize(700,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+    }    
 }
